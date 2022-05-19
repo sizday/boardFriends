5bea8c870f391c42cf03f08138f04f269d461e7e
dsizo <DSizov@rpa-robin.ru>
2022-05-19 13:09:36 +0300

    Loading to server

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,1 @@
-from flask import Flask
-from config import Config
-from app.models import db
-from app.blueprints import blueprint as basic_endpoints
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.register_blueprint(basic_endpoints)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-app.app_context().push()
-# db.drop_all()
-db.create_all()
-db.session.commit()
